50e18f9f142244c76a925797a057ccc1b715ac05
KristianDimitrov <34716572+Kristian8606@users.noreply.github.com>
2018-03-17 00:56:14 +0200

    Add problem

---
+++ intro-and-basic-syntax/exercise/problem-two.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+
+namespace RectangleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double height = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+
+            double sum = height * width;
+
+            Console.WriteLine("{0:F2}", sum);
+        }
+    }
+}
+
