566b3f327e20a73124c567f8a388516979e2c5f2
iani4ka <37180038+iani4ka@users.noreply.github.com>
2018-03-16 20:40:39 +0200

    Update problem-four.cs

--- intro-and-basic-syntax/exercise/problem-four.cs
+++ intro-and-basic-syntax/exercise/problem-four.cs
@@ -6,14 +6,19 @@ namespace P04_BeverageLabels
     {
         public static void Main(string[] args)
         {
-            var product = Console.ReadLine();
+            //We recieve from the Console the product name -> a string;
+            strig product = Console.ReadLine();
+            
+            //In tne next three lines,we recieve volume,energy and sugar content of our product,as and integer;
             var volume = int.Parse(Console.ReadLine());
             var energyPer100ml = int.Parse(Console.ReadLine());
             var sugarPer100ml = int.Parse(Console.ReadLine());
 
+            //We calculate..
             double totalEnergy = energyPer100ml / 100.0 * volume;
             double totalSugar = sugarPer100ml * volume / 100.0;
 
+            //TODO
             Console.WriteLine("{0}ml {1}:", volume, product);
             Console.WriteLine("{0}kcal, {1}g sugars", totalEnergy, totalSugar);
         }
