35aa8666573ffd9579b5d537c08b7dcffde49019
iani4ka <37180038+iani4ka@users.noreply.github.com>
2018-03-16 20:56:13 +0200

    Update problem-one.cs
    Add description of Problem 1

--- intro-and-basic-syntax/exercise/problem-one.cs
+++ intro-and-basic-syntax/exercise/problem-one.cs
@@ -6,8 +6,10 @@ namespace DebitCardNumber
     {
         static void Main(string[] args)
         {
+            //We recieve from the Console the product name -> 4 integers;
             string debitCardNumber = "";
 
+            //We print them in 4-digit debit card format;
             for (int i = 0; i < 4; i++)
             {
                 int number = int.Parse(Console.ReadLine());
