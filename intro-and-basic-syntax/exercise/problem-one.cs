b1c5ee941a59218b0b81e75f4df2db43291baf56
ad2bg <alex.dimitrov.bg@gmail.com>
2018-03-18 18:03:51 +0200

    Changed a variable name in problem-one.cs :   number -> integerNumber

--- intro-and-basic-syntax/exercise/problem-one.cs
+++ intro-and-basic-syntax/exercise/problem-one.cs
@@ -12,8 +12,8 @@ namespace DebitCardNumber
             //We print them in 4-digit debit card format;
             for (int i = 0; i < 4; i++)
             {
-                int number = int.Parse(Console.ReadLine());
-                debitCardNumber += number.ToString().PadLeft(4, '0') + " ";
+                int integerNumber = int.Parse(Console.ReadLine());
+                debitCardNumber += integerNumber.ToString().PadLeft(4, '0') + " ";
             }
             Console.WriteLine(debitCardNumber);
         }
