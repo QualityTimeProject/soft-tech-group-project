e1e221042460bc9df79f6ee0cee7044fdf4e04a4
VladiG1984 <vladig1984@gmail.com>
2018-03-18 14:27:34 +0200

    Changed the variable's name "volume" to "vol" for brevity.

--- intro-and-basic-syntax/exercise/problem-four.cs
+++ intro-and-basic-syntax/exercise/problem-four.cs
@@ -10,16 +10,16 @@ namespace P04_BeverageLabels
             var product = Console.ReadLine();
 
             //In the next three lines,we recieve volume,energy and sugar content of our product,as and integer;
-            var volume = int.Parse(Console.ReadLine());
+            var vol = int.Parse(Console.ReadLine());
             var energyPer100ml = int.Parse(Console.ReadLine());
             var sugarPer100ml = int.Parse(Console.ReadLine());
 
             //We calculate...
-            double totalEnergy = energyPer100ml / 100.0 * volume;
-            double totalSugar = sugarPer100ml * volume / 100.0;
+            double totalEnergy = energyPer100ml / 100.0 * vol;
+            double totalSugar = sugarPer100ml * vol / 100.0;
 
             //TODO
-            Console.WriteLine("{0}ml {1}:", volume, product);
+            Console.WriteLine("{0}ml {1}:", vol, product);
             Console.WriteLine("{0}kcal, {1}g sugars", totalEnergy, totalSugar);
         }
     }
