46495c1662e501a939122161efe8e581e6986049
VladiG1984 <vladig1984@gmail.com>
2018-03-18 14:39:52 +0200

    Confirmed that solution to problem five is correct as per homework requirements (section e).

--- intro-and-basic-syntax/exercise/problem-five.cs
+++ intro-and-basic-syntax/exercise/problem-five.cs
@@ -1,10 +1,11 @@
-﻿using System;
+//CONFIRMED from VladiG1984
+using System;
 
 namespace CharacterStats
 {
     class Program
     {
-		// Define a method that creates a string with data about the video game character's current state relative to its maximum state.
+	// Define a method that creates a string with data about the video game character's current state relative to its maximum state.
         static string PrintCurrentToMaxState(int current, int max)
         {
             string currentToMaxState = '|' + new string('|', current) +
@@ -14,14 +15,14 @@ namespace CharacterStats
 
         static void Main(string[] args)
         {
-			// Read data about video game character from console.
+	    // Read data about video game character from console.
             string name = Console.ReadLine();
             int currentHealth = int.Parse(Console.ReadLine());
             int maxHealth = int.Parse(Console.ReadLine());
             int currentEnergy = int.Parse(Console.ReadLine());
             int maxEnergy = int.Parse(Console.ReadLine());
 
-			// Output data about character on console as follows: character's name, health, and energy.
+	    // Output data about character on console as follows: character's name, health, and energy.
             Console.WriteLine($"Name: {name}");
             Console.WriteLine("Health: " + PrintCurrentToMaxState(currentHealth, maxHealth));
             Console.WriteLine("Energy: " + PrintCurrentToMaxState(currentEnergy, maxEnergy));
